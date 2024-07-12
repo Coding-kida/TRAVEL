ba34a67076bb082c4fd65b853f608a91705ed186
Coding-kida <dhinkachi36@gmail.com>
2024-07-12 22:40:39 +0530

    Add files via upload

---
+++ trip_book.js
@@ -0,0 +1,5 @@
+let bot1 = document.getElementById("#book_now");
+let bot2 = document.getElementById("#trip_plan");
+let bot3 = document.getElementById("#memories");
+
+bot1.onclick="window.open('http://127.0.0.1:5500/TRAVEL/contact.html')"
